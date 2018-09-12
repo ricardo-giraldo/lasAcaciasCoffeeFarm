f604b997cf03374c9eba12433570798354e05a35
Ricardo Giraldo Ramírez <ragiraldor@uqvirtual.edu.co>
2018-09-12 00:11:32 -0500

    Correccion de modelos y migraciones

---
+++ resources/views/inicioSesion.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio de sesi&oacute;n</title>
+</head>
+<body>
+	<h3>Iniciar sesi&oacute;n</h3>
+	<label>usuario</label>
+	<input type="" name="">
+	<label>Contrase&ntilde;a</label>
+	<input type="" name="">
+	<button>Iniciar sesi&oacute;n</button>
+</body>
+</html>
